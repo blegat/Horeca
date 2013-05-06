3c4e32a010b51e501a7feafd4c5029b6009981c9
Van Malleghem Antoine <a.vanmalleghem@hotmail.com>
2013-05-06 10:44:15 +0200

    #1 Mise en place de la mise en page pour SignInActivity

--- src/com/horeca/SignInActivity.java
+++ src/com/horeca/SignInActivity.java
@@ -1,5 +1,12 @@
 package com.horeca;
 
-public class SignInActivity {
+import android.app.Activity;
+import android.os.Bundle;
 
+
+public class SignInActivity extends Activity {
+	 protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_signin);
+	 }
 }
