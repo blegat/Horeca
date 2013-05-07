8b5ed37296cee270c745d4895964ba23c5c7a8aa
Van Malleghem Antoine <a.vanmalleghem@hotmail.com>
2013-05-07 12:08:16 +0200

    SignUp Activity #1

--- src/com/horeca/SignUpActivity.java
+++ src/com/horeca/SignUpActivity.java
@@ -9,13 +9,29 @@ import android.widget.TextView;
 
 public class SignUpActivity extends MyActivity{
 	private Button button = null;
+	private EditText email = null;
+	private EditText name = null;
+	private EditText password = null;
+	private EditText passwordConfirmation = null;
+	SQLiteDatabase db = null;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		MySqliteHelper sqliteHelper = new MySqliteHelper(this);
+		db = sqliteHelper.getReadableDatabase();
+		email = (EditText) findViewById(R.id.mail);
+		name = (EditText) findViewById(R.id.name);
+		password = (EditText) findViewById(R.id.mot_de_passe);
+		passwordConfirmation = (EditText) findViewById(R.id.password_confirmation);
 	    setContentView(R.layout.activity_signup);
 	    button = (Button) findViewById(R.id.sign_up_button);
 	    button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
-        		
+            	String new_name = name.getText().toString();
+            	String new_email = email.getText().toString();
+            	String new_psw = password.getText().toString();
+            	String new_pswC = passwordConfirmation.getText().toString();
+            	
+        		User.signUp(db, new_email, new_name, new_psw, new_pswC);
         		}
         });
 	}
