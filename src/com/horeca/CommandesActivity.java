4d6019ee7bba205d48bffb16b12c9f4f2ada66a5
vladarS <somers.vladimir@gmail.com>
2013-05-08 00:00:43 +0200

    Changement de l'affichage de la list des commandes

--- src/com/horeca/CommandesActivity.java
+++ src/com/horeca/CommandesActivity.java
@@ -3,10 +3,14 @@ package com.horeca;
 import java.util.ArrayList;
 import java.util.Date;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -15,9 +19,9 @@ import android.widget.TextView;
 
 public class CommandesActivity extends MyActivity implements ViewBinder, AdapterView.OnItemClickListener {
 	
-	//private SimpleCursorAdapter adapter = null;
-	private ArrayAdapter<String> adapter2 = null;
-	private ArrayList<String> list_commande = new ArrayList<String>();
+	private CommandeListCursorAdapter adapter = null;
+	//private ArrayAdapter<String> adapter2 = null;
+	//private ArrayList<String> list_commande = new ArrayList<String>();
 	
 	private ListView commandes_list = null;
 	
@@ -44,7 +48,8 @@ public class CommandesActivity extends MyActivity implements ViewBinder, Adapter
 		
 		// Set TextView's content
 		setContentView(R.layout.activity_commandes);
-		
+		Cursor cursor;
+		cursor = Commande.getAllCommandeForPlat(db, plat);
 		commandes_list = (ListView) findViewById(R.id.commandes_list);
 /*
 		adapter = new SimpleCursorAdapter(this, //this context
@@ -58,28 +63,28 @@ public class CommandesActivity extends MyActivity implements ViewBinder, Adapter
 		
 		adapter.setViewBinder(this);
 		*/
-		list_commande = Commande.getAllCommandsTime(db,plat);
-		adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list_commande);
+		//list_commande = Commande.getAllCommandsTime(db,plat);
+		//adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list_commande);
 		
-		commandes_list.setAdapter(adapter2);
-		
+		//commandes_list.setAdapter(adapter2);
+		adapter = new CommandeListCursorAdapter(this, cursor);
+		commandes_list.setAdapter(adapter);
+
 		db.close();
 		
 		commandes_list.setOnItemClickListener(this); 
 	}
-	/*
+	
 	private void refreshCommandesList() {
 		// Open the db
 		MySqliteHelper sqliteHelper = new MySqliteHelper(this);
 		SQLiteDatabase db = sqliteHelper.getReadableDatabase();
 		
-		adapter.changeCursorAndColumns(Commande.getAllCommandeForPlat(db, plat),
-					new String[] { HorecaContract.Commande.NOMBRE },
-					new int[] { android.R.id.text1 });
+		adapter.changeCursor(Commande.getAllCommandeForPlat(db, plat));
 				
 		db.close();
 	}
-	*/
+	
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id)
 	{
@@ -89,7 +94,7 @@ public class CommandesActivity extends MyActivity implements ViewBinder, Adapter
 		Commande commande = new Commande(db, id);
 		commande.destroy(db);
 		db.close();
-		//refreshCommandesList();
+		refreshCommandesList();
 	}
 	
     @Override
@@ -98,4 +103,32 @@ public class CommandesActivity extends MyActivity implements ViewBinder, Adapter
     	((TextView) view).setText(temps.toString());
         return true;
     }
+    
+    
+    
+    
+    public class CommandeListCursorAdapter extends CursorAdapter {
+		
+    	public CommandeListCursorAdapter(Context context, Cursor c) {
+			super(context, c, FLAG_REGISTER_CONTENT_OBSERVER);
+		}
+	 
+		@Override
+		public void bindView(View view, Context context, Cursor cursor) {
+			Commande commande = new Commande(cursor);
+			TextView name = (TextView) view.findViewById(R.id.commande_item_time);
+			name.setText(Utils.dateToString(commande.getTemps()));
+			TextView nombre = (TextView) view.findViewById(R.id.commande_item_nombre);
+			nombre.setText((int)commande.getNombre());
+		}
+	 
+		@Override
+		public View newView(Context context, Cursor cursor, ViewGroup parent) {
+			LayoutInflater inflater = LayoutInflater.from(context);
+			View v = inflater.inflate(R.layout.commande_item, parent, false);
+			bindView(v, context, cursor);
+			return v;
+		}
+    }
 }
+    
