8ad69cf3ebfac965384e94a58ecc787fac9b47c9
Van Malleghem Antoine <a.vanmalleghem@hotmail.com>
2013-05-07 21:26:10 +0200

    MAJ ChangePasswordActivity #1

--- src/com/horeca/ChangePasswordActivity.java
+++ src/com/horeca/ChangePasswordActivity.java
@@ -11,35 +11,44 @@ import android.widget.TextView;
 
 public class ChangePasswordActivity extends MyActivity {
 	private Button button = null;
-	EditText ancien_mdp = null;
-	EditText nouveau_mdp = null;
-	EditText confirmation_mdp = null;
-	TextView password_error = null;
-	private long id;
+	private EditText ancien_mdp = null;
+	private EditText nouveau_mdp = null;
+	private EditText confirmation_mdp = null;
+	private TextView password_error = null;
+	private TextView password_error2 = null;
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_changepassword);
-	    ancien_mdp = (EditText) findViewById(R.id.mail);
-		nouveau_mdp = (EditText) findViewById(R.id.name);
+	    ancien_mdp = (EditText) findViewById(R.id.ancien_mdp);
+		nouveau_mdp = (EditText) findViewById(R.id.nouveau_mdp);
 		confirmation_mdp = (EditText) findViewById(R.id.password_confirmation);
-		password_error=(TextView) findViewById(R.id.password_error);
+		password_error=(TextView) findViewById(R.id.password_err);
+		password_error2=(TextView) findViewById(R.id.password_err2);
 	    button = (Button) findViewById(R.id.change_pass_button);
 	    button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
-            	if(nouveau_mdp.getText().toString()==confirmation_mdp.getText().toString()){
+            	String ancienmdp = ancien_mdp.getText().toString();
+            	String nouveaumdp = nouveau_mdp.getText().toString();
+            	String confirmmdp = confirmation_mdp.getText().toString();
             	MySqliteHelper sqliteHelper = new MySqliteHelper(ChangePasswordActivity.this);
         		SQLiteDatabase db = sqliteHelper.getReadableDatabase();
-        		ContentValues cv = new ContentValues();
-        		cv.put(HorecaContract.User.PASSWORD, nouveau_mdp.getText().toString());
-        		db.insert(HorecaContract.User.TABLE_NAME, null, cv);
+            	int err = User.getCurrentUser().updatePassword(db, ancienmdp, nouveaumdp, confirmmdp);
+            	if (err == User.INVALID_PASSWORD) {
+        			password_error.setVisibility(View.GONE);
+        		} else if (err == User.PASSWORDS_DONT_MATCH) {
+        			password_error.setVisibility(View.VISIBLE);
+        			password_error2.setVisibility(View.GONE);
+        		} else {
+        			password_error.setVisibility(View.GONE);
+        			password_error2.setVisibility(View.VISIBLE);
+        			finish();
+        		}
+            	
         		db.close();
         		finish();
-            	}
-            	else{
-            		password_error.setVisibility(View.VISIBLE);
-            	}
             }
-	    });
+            });
 	}
 }
 
