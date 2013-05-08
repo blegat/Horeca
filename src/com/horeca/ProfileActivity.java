c854cdf3accbd53ae0a7da2b98b65108f57e1d01
Van Malleghem Antoine <a.vanmalleghem@hotmail.com>
2013-05-08 12:21:45 +0200

    MAJ Profile

--- src/com/horeca/ProfileActivity.java
+++ src/com/horeca/ProfileActivity.java
@@ -10,6 +10,7 @@ public class ProfileActivity extends MyActivity {
 	private TextView name = null;
 	private TextView mail = null;
 	private TextView address = null;
+	private TextView ville = null;
 	private Button button = null;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,6 +21,8 @@ public class ProfileActivity extends MyActivity {
 		mail.setText(User.getCurrentUser().getEmail());
 		address = (TextView) findViewById(R.id.show_address);
 		address.setText(User.getCurrentUser().getAddress());
+		ville = (TextView) findViewById(R.id.show_ville);
+		ville.setText(User.getCurrentUser().getVille().getName());
 	    button = (Button) findViewById(R.id.Change_psw);
 	    button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
